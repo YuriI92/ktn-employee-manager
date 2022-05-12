6ad08720cffd1ca9d30b089536c71bd9ac5d3254
Yurippe <yurichikawa1992@gmail.com>
2022-05-12 15:29:00 -0400

    add employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,11 @@ VALUES
     ('Sales Manager', '68233', 1),
     ('Project Engineer', '87500', 2),
     ('System Engineer', '85281', 2);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Yuri', 'Ichikawa', 1, NULL),
+    ('Leona', 'Lewis', 2, NULL),
+    ('Chris', 'Castillo', 4, NULL),
+    ('Joshua', 'Simpson', 3, NULL),
+    ('Jin', 'Kang', 4, NULL);
