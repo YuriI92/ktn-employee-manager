128ebe1e4c5f67e1928f5d039fb3b7ac9aa15057
Yurippe <yurichikawa1992@gmail.com>
2022-05-12 21:24:25 -0400

    add post employee route

--- routes/api/employeeRoutes.js
+++ routes/api/employeeRoutes.js
@@ -24,8 +24,8 @@ router.get('/', (req, res) => {
 
 // add a role
 router.post('/', ({ body }, res) => {
-    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
-    const params = [body.title, body.salary, body.department_id];
+    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
 
     db.query(sql, params, (err, results) => {
         if (err) {
